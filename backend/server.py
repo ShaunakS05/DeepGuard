97edcce687d38acc6c5f669ca4337abcebf97ee4
Shaunak <shaunakduude@gmail.com>
2024-03-23 17:11:25 -0400

    Pushing server changes with integration to frontend

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
 from audioDetection import audioDetection1
 from texttospeech import extractSpeech
@@ -23,11 +23,13 @@ def home():
     return {"Data": "Test"}
 
 @app.post("/check-audio-deepfake")
-def check_audio_deepfake(mp4audio):
-    video = VideoFileClip(mp4audio)
-    video.audio.write_audiofile("output.mp3")
-    result= audioDetection1("output.mp3")
-    return {"result": result}
+async def check_audio_deepfake(mp4video: UploadFile = File(...)):
+    contents = await mp4video.read()
+    with open(mp4video.filename, "wb") as f:
+        video = VideoFileClip(mp4video.filename)
+        video.audio.write_audiofile("output.mp3")
+        result= audioDetection1("output.mp3")
+        return {"result": result}
 
 @app.post("/check-text-deepfake")
 def check_text_deepfake(context):
@@ -37,15 +39,16 @@ def check_text_deepfake(context):
 
 
 @app.post("/check-visual-deepfake")
-def check__visual_deepfake(video):
-    url = "https://ping.arya.ai/api/v1/deepfake-detection"
-    with open(video, "rb") as video_file:
-        video_data = video_file.read()
-    base64_data = base64.b64encode(video_data).decode("utf-8")
-    payload = {"doc_base64": str(base64_data), "req_id": "Detecting_Deepfake", "isIOS": False, "doc_type": "video", "orientation": 0, }
-    headers = {
-        'token': '9b24ff9ff5673c93f425b7e44b80fb1e',
-        'content-type': 'application/json'
-    }
-    response = requests.request("POST", url, json=payload, headers=headers)
-    return {"Data:": response.text}
+async def check__visual_deepfake(video_file: UploadFile = File(...)):
+    contents = await video_file.read()
+    with open(video_file.filename, "wb") as f:
+        url = "https://ping.arya.ai/api/v1/deepfake-detection"
+        video_data = f.read()
+        base64_data = base64.b64encode(video_data).decode("utf-8")
+        payload = {"doc_base64": str(base64_data), "req_id": "Detecting_Deepfake", "isIOS": False, "doc_type": "video", "orientation": 0, }
+        headers = {
+            'token': '9b24ff9ff5673c93f425b7e44b80fb1e',
+            'content-type': 'application/json'
+        }
+        response = requests.request("POST", url, json=payload, headers=headers)
+        return {"Data:": response.text}
