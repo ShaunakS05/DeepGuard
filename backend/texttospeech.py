5c6f2a976d9bf2753166176145a371c6275192ef
Shaunak <shaunakduude@gmail.com>
2024-03-23 16:36:53 -0400

    Sending Data to Brennan

--- backend/texttospeech.py
+++ backend/texttospeech.py
@@ -2,24 +2,26 @@ from google.cloud import speech
 
 client = speech.SpeechClient.from_service_account_file('keygoogle.json')
 
-file_name = "Recording2.mp3"
+file_name = "output.mp3"
 
-with open(file_name, 'rb') as f:
-    mp3_data = f.read()
+def extractSpeech():
 
-
-audio_file = speech.RecognitionAudio(content=mp3_data)
-
-config = speech.RecognitionConfig(
-    sample_rate_hertz=44100,
-    enable_automatic_punctuation=True,
-    language_code='en-US'
-)
+    with open(file_name, 'rb') as f:
+        mp3_data = f.read()
 
 
-response = client.recognize(
-    config=config,
-    audio=audio_file
-)
+    audio_file = speech.RecognitionAudio(content=mp3_data)
 
-print(response)
+    config = speech.RecognitionConfig(
+        sample_rate_hertz=44100,
+        enable_automatic_punctuation=True,
+        language_code='en-US'
+    )
+
+
+    response = client.recognize(
+        config=config,
+        audio=audio_file
+    )
+
+    return response
